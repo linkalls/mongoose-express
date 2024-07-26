ca4f6f9626921fb6fc39d8a3c5ef58bb2d37aa97
poteto <naonyan1135@gmail.com>
2024-07-26 11:08:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const mongoose = require("mongoose")
+
+
+mongoose
+  .connect("mongodb://localhost:27017/express-mongodb1", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("接続ok")
+  })
+  .catch((error) => console.log("MongoDbエラー", error))
+
+
+app.set("views", path.join(__dirname, "views"))
+
+app.set("view engine", "ejs")
+
+
+
+
+
+app.get("/dogs",(req,res)=>{
+  res.send("dogs")
+})
+
+app.listen(3000, () => {
+  console.log("port3000")
+})
