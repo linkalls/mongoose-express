14cb6c1e0d728965fbb7e63ed3c46796b852f65f
poteto <naonyan1135@gmail.com>
2024-07-29 01:59:19 +0900

    ここまでは簡単

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ app.post("/farms", async (req, res) => {
   res.redirect("/farms")
 })
 
+app.get("/farms/:id",async (req,res)=>{
+  const {id} = req.params
+const farm = await Farm.findById(id)
+res.render("farms/show",{farm})
+})
+
 //
 
 const categories = ["果物", "野菜", "乳製品"]
